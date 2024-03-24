f18a29c6ef125a95a270318f77e046acd2354bf8
Jazereel Goh <github@reikolydia.sg>
2024-03-25 00:52:49 +0800

    Create run_venv.ps1

---
+++ run_venv.ps1
@@ -0,0 +1,20 @@
+Clear-Host
+
+. "C:\Program Files\Python310\python.exe" -m venv google_pro_cli
+
+$venvpath = $pwd.path + "\google_pro_cli\Scripts\Activate.ps1"
+
+. $venvpath
+
+pip install -r requirements.txt
+python -m pip install --upgrade pip
+
+$api_key = Read-Host "Input Google AI Studio API key"
+
+$env = "GENAI_API_KEY=" + $api_key
+
+$env > .env
+
+Clear-Host
+
+python main.py
